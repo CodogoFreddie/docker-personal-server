d10e7771c91bc4df6ed320c95ea0691344758368
Freddie Ridell <contact@freddieridell.com>
2018-11-01 20:29:44 +0000

    why u not work

--- updateAndRun.sh
+++ updateAndRun.sh
@@ -4,9 +4,9 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
 
 cd $DIR
 
-git reset --hard HEAD
-git checkout master
-git pull
+git reset --hard HEAD ;
+git checkout master ;
+git pull ;
 
-docker-compose pull
-docker-compose up --build
+docker-compose pull ;
+docker-compose up --build ;
